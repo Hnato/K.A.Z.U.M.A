f5c1ae670d5016e75f3b1802f9fa4c475e7d17ca
Hnato <a.hnatko2077@o2.pl>
2026-04-13 16:14:16 +0200

    update
    Co-Authored-By: ThomasWack <264466934+ThomasWack@users.noreply.github.com>

--- Engine/KAZUMA.Memory/obj/Debug/net11.0/KAZUMA.Memory.AssemblyInfo.cs
+++ Engine/KAZUMA.Memory/obj/Debug/net11.0/KAZUMA.Memory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KAZUMA.Memory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf07fa5ec3f15e9700f516f44c070b4b7c25d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("KAZUMA.Memory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KAZUMA.Memory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
